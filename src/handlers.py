2a9f98a1761ad402d97110e36a9062a645b0264b
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-07-03 15:42:08 +0300

    update images

--- src/handlers.py
+++ src/handlers.py
@@ -1,20 +1,13 @@
 from aiogram import types
-from aiogram.types import CallbackQuery
+from aiogram.types import FSInputFile
+from src.keyboard import get_main_keyboard
 
-from src.keyboard import get_main_keyboard, get_submenu_keyboard
+categories = ["Документы", "Учебный процесс", "Служба заботы", "Другое"]
 
-responses = {
-    "doc_certificates": "Информация будет",
-    "doc_statements": "Информация будет",
-    "doc_orders": "Информация будет",
-    "doc_accepts": "Информация будет",
-    "doc_money": "Информация будет",
-    "study_schedule": "Расписание занятий доступно на сайте.",
-    "study_exams": "Ближайшие экзамены пройдут через месяц.",
-    "support_tech": "Техподдержка работает с 9:00 до 18:00.",
-    "support_psych": "Психолог доступен по записи.",
-    "other_feedback": "Спасибо за обратную связь!",
-    "other_questions": "Задайте ваши вопросы здесь.",
+category_pictures = {
+    "Документы": "images/documents.jpg",
+    "Учебный процесс": "images/study.jpg",
+    "Служба заботы": "images/support.jpg",
 }
 
 async def start_handler(message: types.Message):
@@ -23,28 +16,14 @@ async def start_handler(message: types.Message):
         reply_markup=get_main_keyboard()
     )
 
-async def callback_handler(callback: CallbackQuery):
+async def callback_handler(callback: types.CallbackQuery):
     data = callback.data
 
-    if data == "back_to_main":
-        await callback.message.edit_text(
-            f"Привет, {callback.from_user.full_name}!\nВыбери категорию:",
-            reply_markup=get_main_keyboard()
-        )
+    if data in categories:
+        photo_path = category_pictures.get(data)
+        if photo_path:
+            photo = FSInputFile(photo_path)  
+            await callback.message.answer_photo(photo)
         await callback.answer()
-        return
-
-    if data in ["cat_documents", "cat_study", "cat_support", "cat_other"]:
-        await callback.message.edit_text(
-            "Вы выбрали категорию. Пожалуйста, выберите один из вариантов ниже:",
-            reply_markup=get_submenu_keyboard(data)
-        )
-        await callback.answer()
-        return
-
-    if data in responses:
-        await callback.message.answer(responses[data])
-        await callback.answer()
-        return
-
-    await callback.answer("Неизвестная команда.", show_alert=True)
+    else:
+        await callback.answer("Неизвестная команда", show_alert=True)
