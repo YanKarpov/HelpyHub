bdb40aa6f1c195ad297f7c0fc2de32a0567c6e26
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-07-03 15:49:39 +0300

    добавление поясняющих предложений

--- src/handlers.py
+++ src/handlers.py
@@ -8,6 +8,14 @@ category_pictures = {
     "Документы": "images/documents.jpg",
     "Учебный процесс": "images/study.jpg",
     "Служба заботы": "images/support.jpg",
+    "Другое": "images/other.jpg",
+}
+
+category_texts = {
+    "Документы": "Тебе поможет учебный отдел, он находится 4 этаже рядом с кабинетом 4.2",
+    "Учебный процесс": "Ты можешь обратиться на свою кафедру. Они находятся на 4 этаже напротив столовой",
+    "Служба заботы": "Обратись в кабнет службы заботы на 3 этаже рядом с кабинетом 3.8",
+    "Другое": "Разные полезные сведения.",
 }
 
 async def start_handler(message: types.Message):
@@ -21,9 +29,13 @@ async def callback_handler(callback: types.CallbackQuery):
 
     if data in categories:
         photo_path = category_pictures.get(data)
+        text = category_texts.get(data, "")
         if photo_path:
-            photo = FSInputFile(photo_path)  
-            await callback.message.answer_photo(photo)
+            photo = FSInputFile(photo_path)
+            await callback.message.answer_photo(photo, caption=text)
+        else:
+            # Если фото нет, просто отправляем текст
+            await callback.message.answer(text)
         await callback.answer()
     else:
         await callback.answer("Неизвестная команда", show_alert=True)
