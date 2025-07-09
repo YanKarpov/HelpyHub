30cd8e1c940ce7d9dfb50874555a4e5ac476d61c
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-07-09 14:50:19 +0300

    добавил функция для проверки redis перед запуском бота

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import asyncio
+import sys
 
-from src.bot import dp, bot, register_handlers
+from src.bot import dp, bot, register_handlers, redis_client
 from src.logger import setup_logger
 
 logger = setup_logger(__name__)
 
+async def check_redis():
+    try:
+        pong = await redis_client.ping()
+        if pong:
+            logger.info("Redis доступен и отвечает.")
+        else:
+            logger.error("Redis не отвечает на ping! Ты поднял Docker контейнер?")
+            sys.exit(1)
+    except Exception as e:
+        logger.error(f"Ошибка подключения к Redis: {e}")
+        sys.exit(1)
+
 async def main():
+    await check_redis()  
+
     register_handlers(dp)  
 
     logger.info("Бот запущен и ждёт команды.")
