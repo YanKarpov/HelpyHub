477fcd3b25446479673653ff89ba9a7527ec5ad4
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-08-02 01:09:49 +0300

    Команда /chat_info для отладки и добычи ID каналов

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
 from aiogram.filters import Command
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.types import Message
+from aiogram.filters import Filter
 
 from src.utils.config import BOT_TOKEN
 from src.handlers.start_handler import start_handler
@@ -9,17 +11,13 @@ from src.handlers.feedback_handler import feedback_message_handler
 from src.handlers.admin_handler import admin_reply_text_handler  
 from src.services.redis_client import redis_client
 from src.handlers.admin_commands import block_user_handler, unblock_user_handler
-
-from aiogram.types import Message
-from aiogram.filters import Filter
-
 from src.utils.logger import setup_logger
+
 logger = setup_logger(__name__)
 
 storage = RedisStorage(redis=redis_client)
-
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot=bot, storage=storage) 
+dp = Dispatcher(bot=bot, storage=storage)
 
 class IsAdminReplying(Filter):
     async def __call__(self, message: Message) -> bool:
@@ -27,11 +25,30 @@ class IsAdminReplying(Filter):
         logger.debug(f"IsAdminReplying filter: user_id={message.from_user.id} result={bool(result)}")
         return bool(result)
 
+async def chat_info_handler(message: Message):
+    """Обработчик команды /chat_info"""
+    chat_info = (
+        f"Информация о чате:\n"
+        f"• Тип чата: {'группа' if message.chat.type != 'private' else 'личный'}\n"
+        f"• Chat ID: `{message.chat.id}`\n"
+    )
+    
+    # Логируем информацию
+    logger.info(
+        f"Запрос chat_info: "
+        f"chat_id={message.chat.id}, "
+        f"type={message.chat.type}"
+    )
+    
+    # Отправляем ответ пользователю
+    await message.reply(chat_info, parse_mode="Markdown")
+
 def register_handlers(dp: Dispatcher):
+    # Основные обработчики
     dp.message.register(start_handler, Command(commands=["start"]))
     dp.message.register(block_user_handler, Command(commands=["block_user"]))
     dp.message.register(unblock_user_handler, Command(commands=["unblock_user"]))
+    dp.message.register(chat_info_handler, Command(commands=["chat_info"]))  # Новая команда
     dp.callback_query.register(callback_handler)
     dp.message.register(admin_reply_text_handler, IsAdminReplying())
     dp.message.register(feedback_message_handler)
-    
